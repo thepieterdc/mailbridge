81fd1a4ba2384b24476f56c5d5f949273ed35ae5
Pieter De Clercq <pieterdeclercq@outlook.com>
2018-07-23 16:37:54 +0200

    Main added

---
+++ src/mailbridge.cpp
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2018 - Pieter De Clercq. All rights reserved.
+ *
+ * https://github.com/thepieterdc/mailbridge/
+ */
+
+#include <cstdlib>
+#include <netinet/in.h>
+
+/**
+ * Main entry point.
+ *
+ * @return 0 on success
+ */
+int main() {
+	// Start a socket server on port 25.
+
+	struct sockaddr_in server{};
+
+	return EXIT_SUCCESS;
+}
